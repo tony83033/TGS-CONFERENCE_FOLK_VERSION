d99480d71f828d48c8a4073dcf0c03a841ef89de
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2024-11-13 03:27:10 +0530

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,16 @@ import CityCardGrid from "@/components/customComponents/CityCards";
 import GalleryCarousel from "@/components/customComponents/Gallery";
 import MentorProfile from "@/components/customComponents/Mentor";
 import PricingComponent from "@/components/customComponents/PricingTickets";
+import Faq from "@/components/customComponents/Faq";
 export default function Home() {
   return (
    <>
    <Hero></Hero>
    <CityCardGrid></CityCardGrid>
    <MentorProfile></MentorProfile>
-   <GalleryCarousel></GalleryCarousel>
+   {/* <GalleryCarousel></GalleryCarousel> */}
+   <Faq></Faq>
+   
    <PricingComponent></PricingComponent>
    </>
   );
