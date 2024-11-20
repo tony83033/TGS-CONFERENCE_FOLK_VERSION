3f7e5423acc27a0ff76a6dc6c992b0d52148f746
Sumit <tonystark83033@gmail.com>
2024-11-20 19:52:51 +0530

    refactor:[fix responsivenes of day's button]

--- src/components/customComponents/Shedule.tsx
+++ src/components/customComponents/Shedule.tsx
@@ -17,43 +17,40 @@ const EventSchedule = () => {
         </div>
 
         {/* Day selector buttons */}
-        <div className="flex justify-center gap-4 mb-8">
+        <div className="flex flex-col md:flex-row justify-center gap-4 mb-8">
           <button
             onClick={() => setActiveDay(1)}
-            className={`px-16 py-3 rounded-full text-black font-medium transition-colors ${
-              activeDay === 1
-               
-                 ? "bg-[#ffffff]  shadow-lg shadow-[#00BFFF]/50 scale-105 "
+            className={`px-8 py-2 md:px-16 md:py-3 rounded-full text-black font-medium transition-colors ${activeDay === 1
+                ? "bg-[#ffffff] shadow-lg shadow-[#00BFFF]/50 scale-105"
                 : "bg-[#0A1150] text-white hover:bg-[#4E5A94]"
-            }`}
+              }`}
           >
             Day 1
           </button>
           <button
             onClick={() => setActiveDay(2)}
-            className={`px-16 py-3 rounded-full text-black font-medium transition-colors ${
-              activeDay === 2
+            className={`px-8 py-2 md:px-16 md:py-3 rounded-full text-black font-medium transition-colors ${activeDay === 2
                 ? "bg-[#ffffff] shadow-lg shadow-[#00BFFF]/50 scale-105 text-[#000000]"
                 : "bg-[#0A1150] text-white hover:bg-[#4E5A94]"
-            }`}
+              }`}
           >
             Day 2
           </button>
           <button
             onClick={() => setActiveDay(3)}
-            className={`px-16 py-3 rounded-full text-black font-medium transition-colors ${
-              activeDay === 3
+            className={`px-8 py-2 md:px-16 md:py-3 rounded-full text-black font-medium transition-colors ${activeDay === 3
                 ? "bg-[#ffffff] shadow-lg shadow-[#00BFFF]/50 scale-105 text-[#000000]"
                 : "bg-[#0A1150] text-white hover:bg-[#4E5A94]"
-            }`}
+              }`}
           >
             Day 3
           </button>
         </div>
 
+
         {/* Description text */}
         <p className="text-center text-white italic max-w-3xl mx-auto">
-        Everything About Kubernetes : An Experience Beyond Learning 
+          Everything About Kubernetes : An Experience Beyond Learning
         </p>
       </div>
       {activeDay === 1 && <CourseSchedule />}
