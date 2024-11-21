18ea6a839486ac3a8c9c46011f9522a171a51c61
Sumit <tonystark83033@gmail.com>
2024-11-21 12:09:28 +0530

    added more testimoniol

--- src/components/customComponents/CourseTestimonials.tsx
+++ src/components/customComponents/CourseTestimonials.tsx
@@ -91,8 +91,92 @@ It was an amazing class to attend. I liked the way sir delivered the helm conten
       linkedin: "https://www.linkedin.com/feed/update/urn:li:activity:7239134220567130113/?commentUrn=urn%3Ali%3Acomment%3A%28activity%3A7239134220567130113%2C7240708775475601408%29&dashCommentUrn=urn%3Ali%3Afsd_comment%3A%287240708775475601408%2Curn%3Ali%3Aactivity%3A7239134220567130113%29"
     },
     content: `
-Another amazing informative workshop by vimal sir. It’s always fun learning from you sir. Learned so much of AWS DevOps concepts and how it gets implemented in real-world projects.`,
+Another amazing informative workshop by vimal sir. Its always fun learning from you sir. Learned so much of AWS DevOps concepts and how it gets implemented in real-world projects.`,
     timestamp: "2 month ago"
+  },
+  {
+    id: 8,
+    author: {
+      name: "Rajneesh Pratap Singh",
+      title: "DevOps Engineer || 3x Redhat Certified ||",
+      avatar: "https://media.licdn.com/dms/image/v2/D5603AQFxBq8m8wYj4w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1720442622903?e=1737590400&v=beta&t=v_dQCEGOIDJ96AV0ZbaA0IQUfLmnWk3m1YPnnwXAOb4",
+      linkedin: "https://www.linkedin.com/in/rajneesh-pratap-singh/"
+    },
+    content: `
+This is my first step towards security with DevOps, and it was a great learning experience
+In this session, we covered:
+Why DevSecOps? Understanding its importance over traditional DevOps.
+Integrating security with DevOps, including SAST and secure coding practices.
+Best practices for writing secure Dockerfiles.
+Setting up and using Docker Scout for vulnerability scanning.
+Strategies for patching and managing vulnerabilities in containers..`,
+    timestamp: "1 month ago"
+  },
+
+
+  
+  {
+    id: 10,
+    author: {
+      name: "Aakash Kathunia",
+      title: "Technical Architect at HCLTech | GenAI | LLM | OpenAI | Kubernetes| Automation | Cloud",
+      avatar: "https://media.licdn.com/dms/image/v2/D5603AQGJi9pe0O-O8A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1723740692945?e=1737590400&v=beta&t=y2ONfXKp0z5KkZJRcTtg_e1iVUh8rFUn3JxaRqqFOBg",
+      linkedin: "https://www.linkedin.com/in/aakash-kathunia-6b618b16/"
+    },
+    content: `
+
+    Vimal Sir is outstanding while teaching complex and advanced topics, as he teaches us in such a way that even someone without technical background one can easily grasp and this is the main reason I have been following him since long.
+
+    Before his session, I had tried to understand DevSecOps through various technical channels, but I didn't get a clear picture. However, in just initial hours of Vimal Sir’s session, he made the concept very much clearer. Then, over the next two hours, he simplified DevSecOps further, helping me gain valuable insights that sparked my interest in exploring the topic more deeply.
+    
+    I am Thankful to Vimal Sir for in-depth course on DevSecOps and looking more indepth session from him, and Im grateful for his teachings for current and upcoming valuable training sessions.`,
+    timestamp: "1 month ago"
+  },
+  {
+    id: 11,
+    author: {
+      name: "Alok Kumar Singh ",
+      title: "Web Developer(Angular ) | Data Analyst |",
+      avatar: "https://media.licdn.com/dms/image/v2/D5603AQFGCjRM6gI1zg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724789778105?e=1737590400&v=beta&t=sm-JTK9zKq4_tbEEKi1nEZvWEa4fL-RoAH7KeL86jtk",
+      linkedin: "https://www.linkedin.com/in/tech-alok/"
+    },
+    content: `
+
+Attending the System Design workshop by Vimal Daga Sir was a great learning experience. He explained complex topics like scalability, fault tolerance, and load balancing in simple terms, making it easy to understand. The examples he gave helped us see how these concepts are used in real projects. I learned how to make systems more efficient and reliable by balancing important factors like performance and cost. I'm excited to apply these new skills in my future projects!
+.`,
+    timestamp: "1 month ago"
+  },
+
+  {
+    id: 12,
+    author: {
+      name: "Mallesh Gangadhar ",
+      title: "--",
+      avatar: "https://media.licdn.com/dms/image/v2/D5635AQFvYLlZaIHe_A/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1713248954215?e=1732777200&v=beta&t=TAOMDi9g-MQ4HMspIYg8WBdCJ53UiCsR-c_GIDtjzzA",
+      linkedin: "https://www.linkedin.com/in/mallesh-gangadhar-8ab53777/"
+    },
+    content: `
+
+Superrrrrrrb Training Enjoyed! This Linux Essentials workshop with Vimal Sir was superb, The workshop provided a solid foundation, covering key concepts like processes, users, file operations, and essential commands. It also gave a glimpse into the power and versatility of Linux for solving real-world problems. I'm excited to learn next skills on Linux with AI.
+to enhance my IT skills in Linux! Thanks
+.`,
+    timestamp: "1 month ago"
+  },
+
+  {
+    id: 13,
+    author: {
+      name: "Mohit Agarwal ",
+      title: "Student at NIT Kurukshetra | Additional Secretary of MExperts",
+      avatar: "https://media.licdn.com/dms/image/v2/D5603AQFfS5ahFvJB2Q/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1681871987732?e=1737590400&v=beta&t=xyrWG92tvwvBV-V5WSDeucFFSkLx-j8GbZ3e_1lAjw0",
+      linkedin: "https://www.linkedin.com/in/mohitagarwal99/"
+    },
+    content: `
+
+The workshop was incredibly beneficial! I'm excited about the practical applications of CI/CD and AWS services. I plan to start by working on a personal project to implement a deployment pipeline and use AWS CodeDeploy for automation. Collaborating with others will definitely enhance the learning experience, and I'm eager to stay updated with new DevOps trends to improve my skills continuously.
+I'm really looking forward to applying these insights!
+.`,
+    timestamp: "1 month ago"
   },
   
 ];
