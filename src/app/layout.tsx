188ec75f858d51b7c6e90f5e1c30b61b6ba4561a
Sumit <tonystark83033@gmail.com>
2024-11-22 02:32:14 +0530

    remove particapte button from layout txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/customComponents/Navbar";
 import Footer from "@/components/customComponents/Footer";
 import WhatsAppBot from "@/components/customComponents/WhatsAppBot";
-import ParticipateNowButton from "@/components/customComponents/ParticipateNow";
+//import ParticipateNowButton from "@/components/customComponents/ParticipateNow";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
