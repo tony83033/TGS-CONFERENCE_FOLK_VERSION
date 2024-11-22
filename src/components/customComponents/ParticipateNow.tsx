72f32d4510683cea54550eb815c7a4ba4d6e5a7b
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2024-11-22 10:51:15 +0530

    updated code

--- src/components/customComponents/ParticipateNow.tsx
+++ src/components/customComponents/ParticipateNow.tsx
@@ -5,7 +5,7 @@ const ParticipateNowButton = () => {
   return (
     // Container for the button with Tailwind's responsive utilities
     <div className="fixed bottom-0 left-0 w-full h-[60px] bg-[#ff0000] z-50 flex md:hidden lg:hidden items-center justify-center ">
-     <a href="#buy-tickets"> <Button className="w-full h-full bg-red-600 text-white font-bold text-lg hover:bg-red-700 focus:outline-none">
+     <a href="#buy-tickets"> <Button className="w-full h-full bg-[#ff0000] text-white font-bold text-lg hover:bg-red-700 focus:outline-none">
         Participate Now
       </Button>
       </a>
