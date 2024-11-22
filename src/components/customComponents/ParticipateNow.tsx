87fb24893cbbfcf6771662d63005aa7dea22f503
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2024-11-22 10:47:41 +0530

    refactor:bg color changed and height changed

--- src/components/customComponents/ParticipateNow.tsx
+++ src/components/customComponents/ParticipateNow.tsx
@@ -4,7 +4,7 @@ import { Button } from '../ui/button'; /// Assuming Shadcn is correctly set up a
 const ParticipateNowButton = () => {
   return (
     // Container for the button with Tailwind's responsive utilities
-    <div className="fixed bottom-0 left-0 w-full h-[70px] bg-blue-500 z-50 flex md:hidden lg:hidden items-center justify-center ">
+    <div className="fixed bottom-0 left-0 w-full h-[60px] bg-[#ff0000] z-50 flex md:hidden lg:hidden items-center justify-center ">
      <a href="#buy-tickets"> <Button className="w-full h-full bg-red-600 text-white font-bold text-lg hover:bg-red-700 focus:outline-none">
         Participate Now
       </Button>
